89c347f393bdb6e63e6ac5933f7d0db8b57717a7
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-05-14 11:45:22 +0200

    done

---
+++ pages/articles/_id.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>nice</div>
+</template>
+
+<script>
+import meta from '@/assets/data/meta-tag.json'
+
+export default {
+  data() {
+    return {
+      metaData: meta.metaDetail,
+    }
+  },
+  head() {
+    return {
+      title: this.metaData.title,
+      meta: [
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.metaData.title,
+        },
+        {
+          hid: 'og:url',
+          name: 'og:url',
+          content: this.$route.fullPath,
+        },
+        {
+          hid: 'og:type',
+          name: 'og:type',
+          content: 'article',
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.metaData.description,
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: this.metaData.image,
+        },
+      ],
+    }
+  },
+}
+</script>
